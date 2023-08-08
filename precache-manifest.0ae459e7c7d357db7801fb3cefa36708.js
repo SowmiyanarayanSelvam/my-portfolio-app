58bb40a2ec66e427181c86c3be2f8a460fdc6e0e
Sowmiyanarayan Selvam <55820583+SowmiyanarayanSelvam@users.noreply.github.com>
2023-08-08 13:22:37 +0530

    Updates

---
+++ precache-manifest.0ae459e7c7d357db7801fb3cefa36708.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f51f5b93c57e1e752d21",
+    "url": "/static/css/main.04c8a386.chunk.css"
+  },
+  {
+    "revision": "f51f5b93c57e1e752d21",
+    "url": "/static/js/main.f51f5b93.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "924df6eb7e0efbc85e9b",
+    "url": "/static/js/2.924df6eb.chunk.js"
+  },
+  {
+    "revision": "7f2d4e2d2fa34e27db68",
+    "url": "/static/js/runtime~main.7f2d4e2d.js"
+  },
+  {
+    "revision": "319cdf251fba1d8e697d084a81b2fc16",
+    "url": "/static/media/SelvamS.319cdf25.jpg"
+  },
+  {
+    "revision": "625d350e16165a1ab3d6fb367261439b",
+    "url": "/static/media/Bidsy.625d350e.gif"
+  },
+  {
+    "revision": "8eee73092f227598622e789bc570a7c9",
+    "url": "/static/media/EMS.8eee7309.gif"
+  },
+  {
+    "revision": "f68544e13a460b07d9f705edc7aceb59",
+    "url": "/static/media/SCR-1.f68544e1.gif"
+  },
+  {
+    "revision": "5cbb5eb09e6ed2a13b227fc4c9c2cdfe",
+    "url": "/static/media/SCR-2.5cbb5eb0.gif"
+  },
+  {
+    "revision": "75448fe3df7e3711a8f6ceee95f093ac",
+    "url": "/static/media/basketball.75448fe3.png"
+  },
+  {
+    "revision": "9f1979c4b4899cc27540812d047179be",
+    "url": "/index.html"
+  }
+];
