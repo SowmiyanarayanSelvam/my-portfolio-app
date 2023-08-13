f15aeab9111fdab991c7a1a7ee86972769bf336d
Sowmiyanarayan Selvam <55820583+SowmiyanarayanSelvam@users.noreply.github.com>
2023-08-13 11:34:23 +0530

    Updates

---
+++ precache-manifest.1f3d79a4c3e9f7319f18b3e20e70d37b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50addd5e1a9a898291b",
+    "url": "/my-portfolio-app/static/css/main.8b937496.chunk.css"
+  },
+  {
+    "revision": "d50addd5e1a9a898291b",
+    "url": "/my-portfolio-app/static/js/main.581f2ce3.chunk.js"
+  },
+  {
+    "revision": "377b2b1b4a14b1fdcfc6",
+    "url": "/my-portfolio-app/static/js/runtime~main.b341c981.js"
+  },
+  {
+    "revision": "c69abf02ff52efb7ef81",
+    "url": "/my-portfolio-app/static/js/2.5fd69eb2.chunk.js"
+  },
+  {
+    "revision": "1d7a7846e69ca36444bd",
+    "url": "/my-portfolio-app/static/js/3.1e9de0c8.chunk.js"
+  },
+  {
+    "revision": "319cdf251fba1d8e697d084a81b2fc16",
+    "url": "/my-portfolio-app/static/media/SelvamS.319cdf25.jpg"
+  },
+  {
+    "revision": "625d350e16165a1ab3d6fb367261439b",
+    "url": "/my-portfolio-app/static/media/Bidsy.625d350e.gif"
+  },
+  {
+    "revision": "8eee73092f227598622e789bc570a7c9",
+    "url": "/my-portfolio-app/static/media/EMS.8eee7309.gif"
+  },
+  {
+    "revision": "f68544e13a460b07d9f705edc7aceb59",
+    "url": "/my-portfolio-app/static/media/SCR-1.f68544e1.gif"
+  },
+  {
+    "revision": "5cbb5eb09e6ed2a13b227fc4c9c2cdfe",
+    "url": "/my-portfolio-app/static/media/SCR-2.5cbb5eb0.gif"
+  },
+  {
+    "revision": "75448fe3df7e3711a8f6ceee95f093ac",
+    "url": "/my-portfolio-app/static/media/basketball.75448fe3.png"
+  },
+  {
+    "revision": "2efe6d82b495d108993e7faa9370d446",
+    "url": "/my-portfolio-app/index.html"
+  }
+];
