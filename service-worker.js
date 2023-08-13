a73f8ee35fc26f5a9e9991f40578162da7c0911c
Sowmiyanarayan Selvam <55820583+SowmiyanarayanSelvam@users.noreply.github.com>
2023-08-13 11:32:38 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "www.sowmiselvam.com/precache-manifest.9a58fb43f06e93e8aae0666d6153f236.js"
+  "/precache-manifest.4d221f95408ccc0ddf761703aad7d404.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("www.sowmiselvam.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
