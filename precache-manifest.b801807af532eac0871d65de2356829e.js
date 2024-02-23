bd75b9fe4ba9c7d64905528a391c4d820d996efd
Sowmiyanarayan Selvam <ss4370@scarletmail.rutgers.edu>
2024-02-23 10:53:35 -0500

    Updates

---
+++ precache-manifest.b801807af532eac0871d65de2356829e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7f4e9495c9fdbdb3561",
+    "url": "/my-portfolio-app/static/css/main.dd6e7156.chunk.css"
+  },
+  {
+    "revision": "d7f4e9495c9fdbdb3561",
+    "url": "/my-portfolio-app/static/js/main.0ceeb921.chunk.js"
+  },
+  {
+    "revision": "377b2b1b4a14b1fdcfc6",
+    "url": "/my-portfolio-app/static/js/runtime~main.b341c981.js"
+  },
+  {
+    "revision": "c69abf02ff52efb7ef81",
+    "url": "/my-portfolio-app/static/js/2.5fd69eb2.chunk.js"
+  },
+  {
+    "revision": "1d7a7846e69ca36444bd",
+    "url": "/my-portfolio-app/static/js/3.1e9de0c8.chunk.js"
+  },
+  {
+    "revision": "71ab4afcf13f2369632f43a004635d15",
+    "url": "/my-portfolio-app/static/media/Sowmi2.71ab4afc.jpeg"
+  },
+  {
+    "revision": "625d350e16165a1ab3d6fb367261439b",
+    "url": "/my-portfolio-app/static/media/Bidsy.625d350e.gif"
+  },
+  {
+    "revision": "f68544e13a460b07d9f705edc7aceb59",
+    "url": "/my-portfolio-app/static/media/SCR-1.f68544e1.gif"
+  },
+  {
+    "revision": "5cbb5eb09e6ed2a13b227fc4c9c2cdfe",
+    "url": "/my-portfolio-app/static/media/SCR-2.5cbb5eb0.gif"
+  },
+  {
+    "revision": "ccc30c948cf0a09fe12777933f228849",
+    "url": "/my-portfolio-app/static/media/AR.ccc30c94.gif"
+  },
+  {
+    "revision": "75448fe3df7e3711a8f6ceee95f093ac",
+    "url": "/my-portfolio-app/static/media/basketball.75448fe3.png"
+  },
+  {
+    "revision": "ecddb16ff75fd69b4dd777330b216d80",
+    "url": "/my-portfolio-app/index.html"
+  }
+];
